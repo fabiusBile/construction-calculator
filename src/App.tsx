80607702d85274c7031df468b54c46319f1bbc83
fab <dock234@yandex.ru>
2022-07-15 00:37:57 +0500

    Add navigation, fix letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,208 +1,119 @@
-import React from 'react';
-import ICalculatorBlock from "./calculators/interfaces/ICalculatorBlock";
-import FrameCalculator from "./calculators/frame/FrameCalculator";
-import FrameCalculatorView from "./calculators/frame/FrameCalculatorView";
-import GetFrameMaterials from "./calculators/frame/FrameCalculatorRepository";
+import React, {useEffect} from 'react';
 import {
-    Avatar,
+    AppBar,
     Box,
-    CircularProgress,
-    Container,
-    Divider,
+    Drawer,
+    IconButton,
     List,
-    ListItem,
+    ListItemButton,
     ListItemText,
-    Paper,
-    Typography
+    Toolbar,
+    Typography,
+    useTheme
 } from "@mui/material";
-import {blue} from "@mui/material/colors";
-import {observer} from "mobx-react-lite";
-import {MainTextInput} from "./shared/mainTextInput/MainTextInput";
-import MainTextInputView from "./shared/mainTextInput/MainTextInputView";
-import LettersDimensionsCalculator from "./shared/letters/LettersDimensionsCalculator";
-import getLettersDimensions from "./shared/letters/LettersRepository";
-import {observable} from "mobx";
-import BackCalculator from "./calculators/back/BackCalculator";
-import getBackMaterials from "./calculators/back/BackMaterialRepository";
-import BackCalculatorView from "./calculators/back/BackCalculatorView";
-import getFrontMaterials from "./calculators/front/FrontMaterialRepository";
-import FrontCalculator from "./calculators/front/FrontCalculator";
-import FrontCalculatorView from "./calculators/front/FrontCalculatorView";
-import getSideMaterials from "./calculators/side/SideMateriialRepository";
-import SideCalculator from "./calculators/side/SideCalculator";
-import SideCalculatorView from "./calculators/side/SideCalculatorView";
-import {getDiodes, getPowerSupplies} from "./calculators/diodes/DiodesRepository";
-import DiodesCalculator from "./calculators/diodes/DiodesCalculator";
-import DiodesCalculatorView from "./calculators/diodes/DiodesCalculatorView";
-import loadPrices from "./spreadsheetsInteraction/worksheetsLoader";
-import ceilTo2Decimals from "./shared/ceilTo2Decimals";
+import {NavLink, Route, Routes, useLocation,} from "react-router-dom";
+import VolumetricLightLetters from "./pages/VolumetricLightLetters";
+import MenuIcon from '@mui/icons-material/Menu';
+import {ArrowBack} from "@mui/icons-material";
 
-const rouble = "₽";
-
-const views: React.ReactElement[] = observable([]);
-const calculators: ICalculatorBlock[] = observable([])
-const mainTextInput = new MainTextInput();
-const viewNames: string[] = observable([]);
-
-(async () => {
-    const pricesWorkbook = await loadPrices();
-    const letterDimensions = getLettersDimensions(pricesWorkbook);
-    const lettersCalculator = new LettersDimensionsCalculator(letterDimensions)
-
-    const frameCalculator = new FrameCalculator(GetFrameMaterials(pricesWorkbook))
-    const backMaterials = getBackMaterials(pricesWorkbook);
-    const backCalculator = new BackCalculator(backMaterials, mainTextInput, lettersCalculator);
-
-    const frontMaterials = getFrontMaterials(pricesWorkbook);
-    const frontCalculator = new FrontCalculator(frontMaterials, mainTextInput, lettersCalculator);
-
-    const sideMaterials = await getSideMaterials(pricesWorkbook);
-    const sideCalculator = new SideCalculator(sideMaterials, mainTextInput, lettersCalculator);
-
-    const diodesTypes = getDiodes(pricesWorkbook);
-    const powerSupplies = getPowerSupplies(pricesWorkbook);
-    const diodesCalculator = new DiodesCalculator(diodesTypes, powerSupplies);
-
-    calculators.push(frameCalculator, backCalculator, frontCalculator, sideCalculator, diodesCalculator);
-    views.push(
-        <MainTextInputView mainTextInput={mainTextInput}/>,
-        <FrameCalculatorView calculator={frameCalculator}/>,
-        <BackCalculatorView calculator={backCalculator}/>,
-        <FrontCalculatorView calculator={frontCalculator}/>,
-        <SideCalculatorView calculator={sideCalculator}/>,
-        <DiodesCalculatorView calculator={diodesCalculator} />
-    );
-
-    viewNames.push(
-        "Текст",
-        ...calculators.map((e) => e.name)
-    )
-})();
-
-function priceView({calculators}: { calculators: ICalculatorBlock[] }) {
-    const total = ceilTo2Decimals(calculators.reduce((p, c) => p + (c.getPrice()).price, 0));
-    const totalWithMarkup = ceilTo2Decimals(total + total * (mainTextInput.markup / 100));
-    return (
-        <Box>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                gap: 1,
-                my: 2
-            }}>
-                <Typography variant="h5">Цена</Typography>
-                <Divider sx={{flex: 1}}></Divider>
-            </Box>
-            <Paper square sx={{
-                my: 1,
-                p: 1
-            }}>
-                <Typography component="div">
-                    <List>
-                        {calculators.map((c, ci) => {
-                            const price = c.getPrice();
-                            const detailsKeys = Object.keys(price.details);
-                            return <ListItem key={ci}>
-                                <Box sx={{flex: 1}}>
-                                    <ListItemText>
-                                        {c.name}
-                                    </ListItemText>
-                                    <List>
-                                        {
-                                            detailsKeys.map((key, di) => (
-                                                <ListItem key={di}>
-                                                    <Box sx={{
-                                                        flex: 1,
-                                                        display: 'flex',
-                                                        flexDirection: 'row',
-                                                        alignItems: 'center',
-                                                        gap: 1,
-                                                    }}>
-                                                        {key}
-                                                        <Divider sx={{flex: 1}}></Divider>
-                                                        {price.details[key]} {rouble}
-                                                    </Box>
-                                                </ListItem>
-                                            ))
-                                        }
-                                    </List>
-                                </Box>
-                            </ListItem>
-                        })
-                        }
-                    </List>
-                </Typography>
-            </Paper>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                gap: 1,
-                my: 1
-            }}>
-                <Typography variant="h6">
-                    Итого
-                </Typography>
-                <Divider sx={{flex: 1}}></Divider>
-                <Typography>
-                    {total} {rouble}
-                </Typography>
-            </Box>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                gap: 1,
-                my: 1
-            }}>
-            <Typography variant="h6">
-                Итого с наценкой
-            </Typography>
-            <Divider sx={{flex: 1}}></Divider>
-            <Typography>
-                {totalWithMarkup} {rouble}
-            </Typography>
-            </Box>
-        </Box>
-    )
+interface PageComponent {
+    name: string,
+    component: JSX.Element
 }
 
-const PriceViewObservable = observer(priceView);
+const pages : Record<string, PageComponent> = {
+    "/": {
+        name: "Объемные световые буквы",
+        component: <VolumetricLightLetters/>
+    },
+    "/test": {
+        name: "Test",
+        component: <Box>asd</Box>
+    }
+}
 
 function App() {
-    return views.length > 0 ? (
-        <Container>
-            {views.map((value, index) => (
-                <Box key={index}>
-                    <Box sx={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        gap: 1,
-                        my: 2
-                    }}>
-                        <Avatar sx={{bgcolor: blue[900]}}>{index + 1}</Avatar>
-                        <Typography variant="h6">
-                            {viewNames[index]}
-                        </Typography>
-                        <Divider sx={{flex: 1}}></Divider>
-                    </Box>
-                    <Paper sx={{p: 2}} square>
-                        {value}
-                    </Paper>
-                </Box>
-            ))}
-            <PriceViewObservable calculators={calculators}></PriceViewObservable>
-        </Container>
-    ) : (
-        <Box display="flex" alignItems="center" justifyContent="center" height="100vh" flexDirection="column" gap={1}>
-            <CircularProgress color="secondary"/>
-            <Typography align="center">
-                Загрузка...
-            </Typography>
+
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
+    const [currentPage, setCurrentPage] = React.useState("/")
+    const handleDrawerToggle = () => {
+        setDrawerOpen(!drawerOpen)
+    }
+    const theme = useTheme();
+    const drawerWidth = 250;
+    const location = useLocation();
+
+    useEffect(() => {
+        setCurrentPage(location.pathname)
+    }, [location]);
+
+    return (
+        <Box>
+            <AppBar
+                position="fixed"
+                sx={{
+                    width: drawerOpen ? `calc(100% - ${drawerWidth}px)` : "100%",
+                    ml: drawerOpen ? `${drawerWidth}px` : 0,
+                    transition: theme.transitions.create(['margin', 'width'], {
+                        easing: theme.transitions.easing.easeOut,
+                        duration: theme.transitions.duration.enteringScreen,
+                    }),
+                }}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{mr: 2}}
+                    >
+                        { drawerOpen ? <ArrowBack /> : <MenuIcon/> } 
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        {pages[currentPage].name}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="persistent"
+                anchor="left"
+                open={drawerOpen}
+            >
+                <List>
+                    {Object.entries(pages).map(([link, {name}], i) => (
+                        <ListItemButton component={NavLink} to={link} key={i} selected={currentPage === link}>
+                            <ListItemText>{name}</ListItemText>
+                        </ListItemButton>
+                    ))}
+                </List>
+            </Drawer>
+            <Box sx={{
+                ml: drawerOpen ? `${drawerWidth}px` : 0,
+                mt: 10,
+                transition: theme.transitions.create(['margin'], {
+                    easing: theme.transitions.easing.easeOut,
+                    duration: theme.transitions.duration.enteringScreen,
+                }),
+            }}>
+                <Routes>
+                    {
+                        Object.entries(pages).map(([link, {component}], i) => (
+                            <Route path={link} element={component} key={i}/>
+                        ))
+                    }
+                </Routes>
+            </Box>
         </Box>
-    );
+    )
 }
 
-export default observer(App);
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'normalize.css';
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <HashRouter>
+          <App />
+      </HashRouter>
   </React.StrictMode>
 );
 declare global {
